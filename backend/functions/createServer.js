2e5b979b245350dac91923377cf3e5c11484ebde
Shiehand <andrewshieh7379@gmail.com>
2021-06-16 17:34:10 -0700

    finished basic template lambdas

--- backend/functions/createServer.js
+++ backend/functions/createServer.js
@@ -3,8 +3,28 @@ const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-	console.log(event);
-	const user = event.body.user;
+	console.log(event.body);
+	const { user, serverName } = event.body;
+	const params = {
+		TableName: process.env.serverTable,
+		Item: {
+			user: user,
+			server_name: serverName,
+		},
+	};
+	console.log(params);
 
-	return 'Hello world';
+	try {
+		const writeRes = await ddb.put(params).promise();
+		return {
+			statusCode: 200,
+			body: JSON.stringify(writeRes),
+		};
+	} catch (error) {
+		console.error('Error: ', error);
+		return {
+			statusCode: 400,
+			body: JSON.stringify(error),
+		};
+	}
 };
--- backend/functions/terminateServer.js
+++ backend/functions/terminateServer.js
@@ -1,4 +1,29 @@
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event) => {
-	console.log(event);
-	return "Hello world";
-}
+	console.log(event.body);
+	const { user, serverName } = event.body;
+	const params = {
+		TableName: process.env.serverTable,
+		Key: {
+			user,
+			server_name: serverName,
+		},
+	};
+
+	try {
+		const res = ddb.delete(params).promise();
+		return {
+			statusCode: 200,
+			body: JSON.stringify(res),
+		};
+	} catch (error) {
+		console.error('Error: ', error);
+		return {
+			statusCode: 400,
+			body: JSON.stringify(error),
+		};
+	}
+};
