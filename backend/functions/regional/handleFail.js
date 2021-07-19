7a734b1fddd3b23ecb6cc477a2e4c3617fbc1912
Shiehand <andrewshieh7379@gmail.com>
2021-07-19 09:08:32 -0700

    start implementing handleFail

--- backend/functions/regional/handleFail.js
+++ backend/functions/regional/handleFail.js
@@ -1,3 +1,25 @@
+// const AWS = require('aws-sdk');
+
+// const ddb = new AWS.DynamoDB.DocumentClient({
+// 	region: process.env.centralRegion,
+// });
 exports.handler = async (event) => {
 	console.log(event);
+	// const params = {
+	// 	TableName: process.env.userServerTableName,
+	// 	Key: {
+	// 		user: event.user,
+	// 		serverId: event.serverId,
+	// 	},
+	// 	UpdateExpression:
+	// 		'SET #status = :newStatus, instanceId = :instanceId, #ipAddress = :ip',
+	// 	ReturnValues: 'UPDATED_NEW',
+	// 	ExpressionAttributeValues: {
+	// 		':newStatus': 'running',
+	// 	},
+	// 	ExpressionAttributeNames: {
+	// 		'#status': 'status',
+	// 	},
+	// };
+	// return {};
 };
