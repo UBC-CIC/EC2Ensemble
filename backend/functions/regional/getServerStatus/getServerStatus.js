6160be7225b81b74d130ab95128a38a894755582
Shiehand <andrewshieh7379@gmail.com>
2021-09-08 15:45:08 -0700

    simplify checking ssm agent flow

--- backend/functions/regional/getServerStatus/getServerStatus.js
+++ backend/functions/regional/getServerStatus/getServerStatus.js
@@ -1,22 +1,43 @@
 const AWS = require('aws-sdk');
 
 const ec2 = new AWS.EC2();
+const ssm = new AWS.SSM();
 
 exports.handler = async (event) => {
 	console.log(event);
 
-	const params = {
-		InstanceIds: [event.instanceId],
-	};
-	const res = await ec2.describeInstances(params).promise();
-	console.log(res);
-	const status = res.Reservations[0].Instances[0].State.Name;
-	const ip = res.Reservations[0].Instances[0].PublicIpAddress;
-	const launchTime = res.Reservations[0].Instances[0].LaunchTime;
-	const elapsedTime = new Date() - launchTime;
-	console.log(launchTime);
-	console.log(elapsedTime);
-	console.log({ status, ip });
+	const running = await checkInstanceSSM(event.instanceId);
+	if (running) {
+		const res = await ec2
+			.describeInstances({
+				InstanceIds: [event.instanceId],
+			})
+			.promise();
+		const ip = res.Reservations[0].Instances[0].PublicIpAddress;
+		const launchTime = res.Reservations[0].Instances[0].LaunchTime;
+		const elapsedTime = new Date() - launchTime;
+		console.log(elapsedTime);
+		return { status: 'running', ip };
+	} else {
+		return { status: 'pending', ip: null };
+	}
+};
 
-	return { status, ip };
+const checkInstanceSSM = async (instanceId) => {
+	try {
+		const res = await ssm
+			.describeInstanceInformation({
+				Filters: [{ Key: 'InstanceIds', Values: [instanceId] }],
+			})
+			.promise();
+		console.log(res);
+		if (!res.InstanceInformationList.length) {
+			return false;
+		} else {
+			return true;
+		}
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
 };
--- backend/functions/regional/runJacktrip/runJacktrip.js
+++ backend/functions/regional/runJacktrip/runJacktrip.js
@@ -3,9 +3,6 @@ const ssm = new AWS.SSM();
 
 exports.handler = async (event) => {
 	console.log(event);
-	while (!(await checkInstanceSSM(event.instanceId))) {
-		await sleep(2000);
-	}
 	try {
 		const res = await ssm
 			.sendCommand({
@@ -24,26 +21,3 @@ exports.handler = async (event) => {
 		throw new Error('SendCommand failed: ' + JSON.stringify(error));
 	}
 };
-
-const checkInstanceSSM = async (instanceId) => {
-	try {
-		const res = await ssm
-			.describeInstanceInformation({
-				Filters: [{ Key: 'InstanceIds', Values: [instanceId] }],
-			})
-			.promise();
-		console.log(res);
-		if (!res.InstanceInformationList.length) {
-			return false;
-		} else {
-			return true;
-		}
-	} catch (error) {
-		console.log(error);
-		return false;
-	}
-};
-
-function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
-}
