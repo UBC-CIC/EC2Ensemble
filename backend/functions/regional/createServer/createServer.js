6d4ece0d35118eb0c5f6d416dc0c177ecbed62c5
Shiehand <andrewshieh7379@gmail.com>
2021-10-12 10:51:31 -0700

    vpc implemented

--- backend/functions/regional/createServer/createServer.js
+++ backend/functions/regional/createServer/createServer.js
@@ -31,6 +31,7 @@ exports.handler = async (event) => {
 			},
 		],
 		SecurityGroups: [process.env.ec2SecurityGroup],
+		SubnetId: [process.env.subnetId],
 		IamInstanceProfile: {
 			Name: 'JacktripEC2InstanceProfile',
 		},
